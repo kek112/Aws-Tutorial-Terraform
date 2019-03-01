759c6d33343398f94b26a948ca0542438987c3f3
Karl-Erik Kley <kkley@inoxio.de>
2019-03-01 12:12:05 +0100

    Add security group with  incoming ssh rules for ec2 instance

---
+++ securitygroup.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "ec2_web" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+/*  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = # add a CIDR block here
+  }  */
+
+ /* ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = # add a CIDR block here
+  }*/
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
